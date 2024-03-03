5838a51006aa633db7d1f55b67afc4b197f0f5ff
Sambit Sargam Ekalabya <sambitsargam2003@gmail.com>
2024-03-03 22:37:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@
 //Provide the plugin settings
 $("#countdown").countdown({
         //The countdown end date
-        date: "4 March 2024 12:00:00",
+        date: "4 March 2024 22:00:00",
 
         // on (03:07:52) | off (3:7:52) - two_digits set to ON maintains layout consistency
         format: "on"
